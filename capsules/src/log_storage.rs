d393413593305e33cbe0eb2219cdef23c9b242d3
Tyler Packard <tyler.packard86@gmail.com>
2019-10-31 23:21:58 -0700

    Naive read/write for log storage

--- capsules/src/log_storage.rs
+++ capsules/src/log_storage.rs
@@ -13,7 +13,7 @@ use kernel::ReturnCode;
 enum State {
     Idle,
     Read,
-    //Write,
+    Write,
 }
 
 pub struct LogStorage<'a, F: Flash + 'static, C: LogReadClient + LogWriteClient> {
@@ -24,7 +24,8 @@ pub struct LogStorage<'a, F: Flash + 'static, C: LogReadClient + LogWriteClient>
 
     state: Cell<State>,
     read_offset: Cell<StorageCookie>,
-    write_offset: Cell<StorageCookie>,
+    append_offset: Cell<StorageCookie>,
+    append_wrapped: Cell<bool>,
 
     length: Cell<StorageLen>,
     bytes_remaining: Cell<StorageLen>,
@@ -43,7 +44,8 @@ impl<'a, F: Flash + 'static, C: LogReadClient + LogWriteClient> LogStorage<'a, F
             client: OptionalCell::empty(),
             state: Cell::new(State::Idle),
             read_offset: Cell::new(SEEK_BEGINNING),
-            write_offset: Cell::new(SEEK_BEGINNING), // TODO: need to recover write offset.
+            append_offset: Cell::new(SEEK_BEGINNING), // TODO: need to recover write offset.
+            append_wrapped: Cell::new(false), // TODO: also need to recover this.
             length: Cell::new(0),
             bytes_remaining: Cell::new(0),
         };
@@ -103,6 +105,7 @@ impl<'a, F: Flash + 'static, C: LogReadClient + LogWriteClient> LogRead for LogS
     }
 
     fn seek(&self, offset: StorageCookie) -> ReturnCode {
+        // TODO: no seeking beyond append offset.
         let status = if offset < self.volume.size {
             self.read_offset.set(offset);
             ReturnCode::SUCCESS
@@ -120,22 +123,57 @@ impl<'a, F: Flash + 'static, C: LogReadClient + LogWriteClient> LogRead for LogS
 }
 
 impl<'a, F: Flash + 'static, C: LogReadClient + LogWriteClient> LogWrite for LogStorage<'a, F, C> {
-    fn append(&self, _buffer: &'static mut [u8], _length: usize) -> ReturnCode {
-        // TODO
-        ReturnCode::SUCCESS
+    fn append(&self, buffer: &'static mut [u8], length: usize) -> ReturnCode {
+        // TODO: what should happen if the length of the buffer is greater than the size of the log?
+        if length == 0 {
+            return ReturnCode::SUCCESS;
+        } else if self.state.get() != State::Idle {
+            return ReturnCode::EBUSY;
+        } else if buffer.len() < length {
+            return ReturnCode::EINVAL;
+        }
+
+        // Ensure end of log hasn't been reached. If circular, append_offset should have already
+        // been reset to `SEEK_BEGINNING` in `write_done`.
+        if self.append_offset.get() < self.volume.size {
+            self.length.set(length);
+            // TODO: shouldn't be able to write more bytes than the size of the volume.
+            let bytes_to_append = core::cmp::min(length, self.volume.size - self.append_offset.get());
+            if bytes_to_append < length && self.circular.get() {
+                // Write extends beyond end of circular volume, save number of bytes to write at
+                // the start of the volume.
+                self.bytes_remaining.set(length - bytes_to_append);
+            }
+
+            let status = self.storage.write(
+                buffer,
+                self.volume.base + self.append_offset.get(),
+                bytes_to_append,
+            );
+            if status == ReturnCode::SUCCESS {
+                self.state.set(State::Write);
+            }
+            status
+        } else {
+            debug_assert!(!self.circular.get());
+            ReturnCode::FAIL
+        }
     }
 
     fn current_offset(&self) -> StorageCookie {
-        self.write_offset.get()
+        self.append_offset.get()
     }
 
     fn erase(&self) -> ReturnCode {
-        // TODO
+        self.read_offset.set(0);
+        self.append_offset.set(0);
+        self.append_wrapped.set(false);
+        self.client.map(move |client| client.erase_done(ReturnCode::SUCCESS));
         ReturnCode::SUCCESS
     }
 
     fn sync(&self) -> ReturnCode {
-        // TODO
+        self.client.map(move |client| client.sync_done(ReturnCode::SUCCESS));
         ReturnCode::SUCCESS
     }
 }
@@ -159,7 +197,7 @@ impl<'a, F: Flash + 'static, C: LogReadClient + LogWriteClient> NonvolatileStora
             let bytes_to_read = self.bytes_remaining.get();
             self.bytes_remaining.set(0);
             let status = self.storage.read(
-                buffer,
+                &mut buffer[length..],
                 self.volume.base + self.read_offset.get(),
                 bytes_to_read,
             );
@@ -176,7 +214,38 @@ impl<'a, F: Flash + 'static, C: LogReadClient + LogWriteClient> NonvolatileStora
         }
     }
 
-    fn write_done(&self, _buffer: &'static mut [u8], _length: usize) {
-        // TODO
+    fn write_done(&self, buffer: &'static mut [u8], length: usize) {
+        // Increment write offset by number of bytes written.
+        // TODO: what if first write wrote beyond the end of the log boundaries?
+        let mut append_offset = self.append_offset.get() + length;
+        if append_offset >= self.volume.size && self.circular.get() {
+            // Reset offset to start of volume if circular and the end is reached.
+            append_offset = SEEK_BEGINNING;
+            self.append_wrapped.set(true);
+        }
+        debug_assert!(append_offset <= self.volume.size);
+        self.append_offset.set(append_offset);
+
+        if self.bytes_remaining.get() > 0 && self.circular.get() {
+            // If more bytes need to be written from the top of a circular log, perform another
+            // write.
+            let bytes_to_append = self.bytes_remaining.get();
+            self.bytes_remaining.set(0);
+            let status = self.storage.write(
+                &mut buffer[length..],
+                self.volume.base + self.append_offset.get(),
+                bytes_to_append,
+            );
+            // TODO: better error handling.
+            if status != ReturnCode::SUCCESS {
+                self.state.set(State::Idle);
+            }
+        } else {
+            // Read complete, issue callback.
+            self.state.set(State::Idle);
+            self.client.map(move |client| {
+                client.append_done(buffer, self.length.get(), self.append_wrapped.get(), ReturnCode::SUCCESS)
+            });
+        }
     }
 }
